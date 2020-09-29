670f1254a946619e0431d384540a1910e17078cc
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-09-29 14:59:33 +0200

    fix: render app
    Co-authored-by: Daniel Knapp <knapp@sipgate.de>
    Co-authored-by: Sebastian Köhnen <koehnen@sipgate.de>

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('fax-app', () => App);
+AppRegistry.runApplication('fax-app', {
+  rootTag: document.getElementById('root'),
+});
