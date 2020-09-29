93984929962967b05a034a7c594c011b192231ff
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-09-29 16:17:37 +0200

    fix: use react-select for dropdown
    Co-authored-by: Daniel Knapp <knapp@sipgate.de>
    Co-authored-by: Sebastian Köhnen <koehnen@sipgate.de>

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleSheet, View, ViewStyle} from 'react-native';
+import {View, ViewStyle} from 'react-native';
+import Dropdown from 'react-select';
 
 interface DropDownItem {
   label: string;
@@ -15,58 +16,16 @@ interface Props {
 }
 
 const DropDown = (props: Props) => {
-  function renderWebPicker() {
-    const items = [{label: props.placeholder, value: ''}, ...props.items];
-    return (
-      <View style={styles.androidPicker}>
-        {/*<Picker
-          style={styles.androidPickerText}
-          selectedValue={props.selected}
-          onValueChange={(value: ReactText) => props.onChange(String(value))}>
-          {items.map((item, index) => {
-            return (
-              <Picker.Item label={item.label} value={item.value} key={index} />
-            );
-          })}
-        </Picker>*/}
-        <select>
-          {items.map((item, index) => {
-            return <option label={item.label} value={item.value} key={index} />;
-          })}
-        </select>
-      </View>
-    );
-  }
-
-  return <View style={props.style}>{renderWebPicker()}</View>;
+  return (
+    <View style={props.style}>
+      <Dropdown
+        menuPortalTarget={document.body}
+        isSearchable={false}
+        options={props.items}
+        placeholder={props.placeholder}
+      />
+    </View>
+  );
 };
 
 export default DropDown;
-
-const styles = StyleSheet.create({
-  iosPicker: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    height: 32,
-    borderBottomColor: '#6a6a6a',
-    borderBottomWidth: 1.5,
-    padding: 8,
-  },
-  iosPickerText: {
-    color: 'black',
-  },
-  iosPickerUnselectedText: {
-    color: '#a9a9a9',
-  },
-  androidPicker: {
-    display: 'flex',
-    width: '100%',
-    borderBottomColor: '#6a6a6a',
-    borderBottomWidth: 1.5,
-  },
-  androidPickerText: {
-    color: 'black',
-    fontSize: 16,
-  },
-});
